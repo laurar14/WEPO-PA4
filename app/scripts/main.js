257b930571aae830f6b14d24d93317b56e74f24f
Laura Orsini R Franca <lorsini.franca@gmail.com>
2016-04-08 21:03:58 +0000

    some small stuffs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,4 +22,11 @@ $(function() {
 			levels.removeClass('is-visible');
 			game.start(90);
 		});
+
+		var currentURL = window.location.href;
+		console.log('currentURL: ' + currentURL);
+		if(currentURL !== 'http://127.0.0.1:9000/') {
+			window.location = 'http://127.0.0.1:9000/404.html'
+		}
+		
 });
